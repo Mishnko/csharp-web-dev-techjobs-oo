9cd5349ac7794fd7f69617aa40e3fe37013f7107
Ella Vankirk <ella.vankirk@gmail.com>
2020-12-23 20:24:14 -0600

    Completed Constructor testing

--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -1,13 +1,37 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechJobsOO;
 
 namespace TechJobsTests
 {
     [TestClass]
     public class JobTests
     {
+
+        //Arrange the variables your test requires
+        //Act on the methods your test requires
+        //Assert the anticipated comparison of the expected and actual values
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestSettingJobId()
         {
+            Job new_job = new Job();
+            Job new_job2 = new Job();
+            Assert.IsFalse(new_job.Id == new_job2.Id);
         }
+
+        //Use Assert statements to test that the constructor correctly assigns the value of each field.
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            Job new_job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            //Assert.AreEqual(expected, actual);
+            Assert.AreEqual("Product tester", new_job.Name);
+            Assert.AreEqual("ACME", new_job.EmployerName.Value);
+            Assert.AreEqual("Desert", new_job.EmployerLocation.Value);
+            Assert.AreEqual("Quality control", new_job.JobType.Value);
+            Assert.AreEqual("Persistence", new_job.JobCoreCompetency.Value);
+            Assert.AreEqual(1, new_job.Id);
+        }
+
     }
 }
